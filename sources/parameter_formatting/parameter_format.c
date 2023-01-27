3695fc61b957b2ad83fda62ae9f1482c451f7bd3
Miika Viinikainen <viinikainen.miika@gmail.com>
2023-01-27 16:07:22 +0200

    parameter format logic added, with nested format, needs still memory handling, error handling and testing alot.

--- sources/parameter_formatting/parameter_format.c
+++ sources/parameter_formatting/parameter_format.c
@@ -6,7 +6,7 @@
 /*   By: mviinika <mviinika@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/17 11:40:05 by mviinika          #+#    #+#             */
-/*   Updated: 2023/01/26 21:07:00 by mviinika         ###   ########.fr       */
+/*   Updated: 2023/01/27 15:57:04 by mviinika         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -80,6 +80,44 @@ int	is_in_var_or_env(t_shell *sh, char *var)
 	return (0);
 }
 
+static char	*retokenize(t_shell *sh, char *subst, int *i)
+{
+	char 	*trimmed;
+	int		k;
+	int		j;
+	int		open;
+	char	*fresh;
+
+	k = 0;
+	j = 0;
+	open = 0;
+	(void)sh;
+	trimmed = ft_strtrim(subst);
+	fresh = ft_strnew(ft_strlen(subst));
+	while (trimmed[*i])
+	{
+		fresh[k++] = trimmed[(*i)++];
+		if (fresh[0] == '$')
+		{
+			if (trimmed[*i] == '{')
+				open += 1;
+		}
+		if (trimmed[*i] == '}')
+		{
+			open -= 1;
+			if (open == 0)
+			{
+				fresh[k++] = trimmed[(*i)++];
+				break ;
+			}
+		}
+		if (trimmed[*i] == '$' && !open)
+			break ;
+	}
+	ft_strdel(&trimmed);
+	return (fresh);
+}
+
 
 
 
@@ -113,28 +151,86 @@ char *count_letters(t_shell *sh, char *cmd)
 	ft_strdel(&var);
 	return (ft_itoa(ft_strlen(expanded)));
 }
+static char	*remove_braces(char *str)
+{
+	char	*new;
+	int		i;
+	int		k;
+
+	k = 0;
+	i = 1;
+	str[1] = str[2];
+	str[ft_strlen(str) - 1] = '\0';
+	ft_printf("%s\n", str);
+	while (str[i])
+	{
+		str[i] = str[i + 1];
+		i++;
+	}
+	ft_printf("%s\n", str);
+	new = ft_strdup(str);
+	return (new);
+}
 
 int	param_format(t_shell *sh, char **cmd)
 {
 	int		i;
+	int		k;
+	int		j = 0;
 	char	*expanded;
 	//char	*strip;
 	char	*var;
 	char	*subs;
 	char	op;
 	int		format;
+	char	**list;
+	char	*strip;
 
 	i = -1;
+	k= 0;
 	subs = ft_strnew(1);
 	expanded = ft_strnew(1);
 	var = ft_strnew(1);
 	op = 0;
 	format = -1;
+	list = (char **)ft_memalloc(sizeof(char *) * 100);
+	// strip = remove_braces(cmd);
 	while (cmd[++i])
 	{
 		if (check_syntax(cmd[i]))
 		{
-			expanded = substitute_or_create(sh, cmd[i]);
+			strip = remove_braces(cmd[i]);
+			ft_printf("stripped %s\n", cmd[i]);
+			subs = ft_strdup(ft_strchr(strip, ':') + 1);
+			ft_printf("not in subs %s\n", subs);
+			op = subs[0];
+			subs = (char *)ft_memmove(subs, subs + 1, ft_strlen(subs));
+			ft_printf("not in subs %s\n", subs);
+			var = ft_strndup(strip, ft_strlen(strip) - (ft_strlen(subs)));
+			ft_printf("not in var %s\n", var);
+			while (subs[j])
+			{
+				list[k++] = retokenize(sh, subs, &j);
+			}
+			k = 0;
+			while (list[k])
+				ft_printf("lisdfsddfdsst %s\n", list[k++]);
+			k = 0;
+			while (list[k])
+			{
+				if (ft_strnequ(list[k], "${", 2))
+					expanded = ft_strjoin(expanded, substitute_or_create(sh, list[k++]));
+				else
+					expanded = ft_strjoin(expanded, list[k++]);
+			}
+			ft_printf("final expand %s %s\n",var, expanded);
+			ft_strdel(&subs);
+			subs = ft_strjoin(var, expanded);
+			ft_printf("variable %s final subst %s\n", var, subs);
+			ft_strdel(&expanded);
+			expanded = substitute_or_create(sh, subs);
+			ft_printf("final subst %s\n", expanded);
+			//expanded = substitute_or_create(sh, cmd[i]);
 			// if (ft_strnequ(cmd[i], "${#", 3))
 			// 	expanded = count_letters(sh, strip);
 			// else
@@ -168,11 +264,12 @@ int	param_format(t_shell *sh, char **cmd)
 			// }
 			// else if (expanded[0] == '$')
 			// 	expanded = ft_expansion_dollar(sh, var);
-			ft_printf("Expanded %s\n", expanded);
+			// ft_printf("Expanded %s\n", expanded);
 			ft_strdel(&cmd[i]);
 			cmd[i] = ft_strdup(expanded);
-			// ft_strdel(&var);
-			// ft_strdel(&subs);
+			ft_strdel(&var);
+			ft_strdel(&subs);
+			ft_strdel(&expanded);
 			
 		}
 	}
--- sources/parameter_formatting/substitute_or_create.c
+++ sources/parameter_formatting/substitute_or_create.c
@@ -6,7 +6,7 @@
 /*   By: mviinika <mviinika@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/26 10:38:55 by mviinika          #+#    #+#             */
-/*   Updated: 2023/01/27 13:06:42 by mviinika         ###   ########.fr       */
+/*   Updated: 2023/01/27 16:03:04 by mviinika         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,9 @@ char	*remove_braces(char *str)
 
 	k = 0;
 	i = 1;
+	ft_printf("%c\n", str[1]);
+	if (!str[1] || str[1] != '{')
+		return (str);
 	str[1] = str[2];
 	str[ft_strlen(str) - 1] = '\0';
 	ft_printf("%s\n", str);
@@ -41,7 +44,7 @@ char	*subst_param(t_shell *sh, char *var, char *subst, int format)
 	expanded = NULL;
 	temp = (char **)ft_memalloc(sizeof(char *) * 2);
 	temp[0] = ft_expansion_dollar(sh, var);
-	ft_printf("temp [%s]\n", temp[0]);
+	//ft_printf("temp [%s]\n", temp[0]);
 	temp[1] = NULL;
 	if (format == 0)
 	{
@@ -58,7 +61,8 @@ char	*subst_param(t_shell *sh, char *var, char *subst, int format)
 		if (!*temp[0])
 		{
 			expanded = ft_strdup(subst + 1);
-			ft_strdel(&temp[0]);
+			//ft_strdel(&temp[0]);
+			//temp[0] = ft_strjoin(var + 1, "=");
 			temp[0] = ft_strjoin(var + 1, subst);
 			ft_printf("temp [%s]\n", temp[0]);
 			add_var(sh, temp);
@@ -68,7 +72,7 @@ char	*subst_param(t_shell *sh, char *var, char *subst, int format)
 	}
 	else if (format == 2)
 	{
-		ft_printf("questionmark[%s]\n", var);
+	//	ft_printf("questionmark[%s]\n", var);
 		if (!*temp[0] && subst[1])
 			ft_printf("42sh: %s: %s\n", var + 1, subst + 1);
 		else if (!*temp[0] && !subst[1])
@@ -81,8 +85,9 @@ char	*subst_param(t_shell *sh, char *var, char *subst, int format)
 		ft_printf("plus sign[%s] %s\n", var, subst);
 		if (temp[0][0])
 		{
+			ft_printf("temp %s plus sign[%s] %s\n",temp[0], var, subst);
 			if (subst && *subst)
-				expanded = ft_strdup(subst);
+				expanded = ft_strdup(subst + 1);
 			else
 				expanded = ft_strnew(1);
 		}
@@ -170,22 +175,27 @@ char *substitute_or_create(t_shell *sh, char *cmd)
 	list = (char **)ft_memalloc(sizeof(char *) * 100);
 	// if (ft_strnequ(cmd, "${#", 3))
 	// 		expanded = count_letters(sh, strip);
+	ft_printf("sisaantuleva matsku [%s]\n", strip);
 	ft_printf("sisaantuleva matsku [%s]\n", cmd);
 	if (ft_strchr(strip, ':'))
 	{
-		subs = ft_strdup(ft_strchr(strip, ':') + 1);
-		op = subs[0];
+		ft_printf("stripped [%s]\n",strip);
+		subs = ft_strdup(ft_strchr(strip, ':'));
+		ft_printf("substitution [%s]\n",subs);
 		subs = (char *)ft_memmove(subs, subs + 1, ft_strlen(subs));
-		var = ft_strndup(strip, ft_strlen(strip) - (ft_strlen(subs) - 1));
-		ft_printf("var [%s] format [%s]\n", var, subs);
-		while (subs[i])
-		{
-			list[k++] = retokenize(sh, subs, &i);
-		}
-		k = 0;
-		while (list[k])
-			ft_printf("list %s\n", list[k++]);
-		exit(1);
+		ft_printf("substitution [%s]\n",subs);
+		op = subs[0];
+		ft_printf("substitution [%s]\n",subs);
+		var = ft_strndup(strip, ft_strlen(strip) - (ft_strlen(subs)) - 1);
+		ft_printf("var [%s] substitution [%s]\n", var, subs);
+		// while (subs[i])
+		// {
+		// 	list[k++] = retokenize(sh, subs, &i);
+		// }
+		// k = 0;
+		// while (list[k])
+		// 	ft_printf("list %s\n", list[k++]);
+		// exit(1);
 	}
 	else if (ft_strchr(strip, '#'))
 	{
@@ -202,12 +212,12 @@ char *substitute_or_create(t_shell *sh, char *cmd)
 		expanded = ft_expansion_dollar(sh, strip);
 		ft_printf("tassa\n");
 	}
-	ft_printf("expanded asdfasfasfasfd[%s]\n", expanded);
+//	ft_printf("expanded asdfasfasfasfd[%s]\n", expanded);
 	if (ft_strnequ(subs, "${", 2))
 		subs = substitute_or_create(sh, subs);
 	format = format_mode(op);
 	if (!*expanded)
 		expanded = subst_param(sh, var, subs, format);
-	ft_printf("expanded [%s]\n", expanded);
+	//ft_printf("expanded [%s]\n", expanded);
 	return (expanded);
 }
