5bf696fc02173e134513fef9b4958602212880e2
jniemine <jniemine@student.hive.fi>
2023-01-27 15:12:56 +0200

    Fixed logicalop logic and a dispatcher bug from ft_test

--- sources/exec_tree/exe_logicalop.c
+++ sources/exec_tree/exe_logicalop.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   exe_logicalop.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mrantil <mrantil@student.hive.fi>          +#+  +:+       +#+        */
+/*   By: jniemine <jniemine@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/12 19:00:00 by jniemine          #+#    #+#             */
-/*   Updated: 2023/01/26 09:56:21 by mrantil          ###   ########.fr       */
+/*   Updated: 2023/01/27 15:08:00 by jniemine         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,15 +16,16 @@ static void	exe_logical_and(t_logicalop *logicalop, char ***environ_cp,
 		char *terminal, t_shell *sh)
 {
 	exec_tree(logicalop->left, environ_cp, terminal, sh);
-	if (sh->exit_stat == 0)
+	if (logicalop->right && logicalop->right->type == LOGICAL_OR)
 		exec_tree(logicalop->right, environ_cp, terminal, sh);
 }
 
 static void	exe_logical_or(t_logicalop *logicalop, char ***environ_cp,
 		char *terminal, t_shell *sh)
 {
-	exec_tree(logicalop->left, environ_cp, terminal, sh);
-	if (sh->exit_stat != 0)
+	if (sh->exit_stat == 0)
+		exec_tree(logicalop->left, environ_cp, terminal, sh);
+	else if (logicalop->right && logicalop->right->type == CMD)
 		exec_tree(logicalop->right, environ_cp, terminal, sh);
 }
 
