a523a0510d67a07bd369f8ea8ce4d88466afa74a
Simo Puustinen <puustinen.simo@gmail.com>
2023-01-29 17:38:34 +0200

    adding argument support for 'exit', -n for echo

--- sources/builtins/ft_echo.c
+++ sources/builtins/ft_echo.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_echo.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mbarutel <mbarutel@student.42.fr>          +#+  +:+       +#+        */
+/*   By: spuustin <spuustin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/26 20:33:02 by mbarutel          #+#    #+#             */
-/*   Updated: 2023/01/05 16:54:46 by mbarutel         ###   ########.fr       */
+/*   Updated: 2023/01/25 19:13:15 by spuustin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,17 +19,20 @@
  * @param cmd The command line arguments.
  * @param nl_flag if true, don't print a newline at the end of the output
  */
-static void	echo_print(char **cmd, bool nl_flag)
+static void	echo_print(char **cmd, int offset, int flag)
 {
-	while (*cmd)
+	int		i;
+
+	i = 1;
+	while (cmd[i + offset])
 	{
-		if (*cmd)
-			ft_putstr(*cmd);
-		cmd++;
-		if (*cmd)
+		if (cmd[i + offset])
+			ft_putstr(cmd[i + offset]);
+		i++;
+		if (cmd[i + offset])
 			ft_putstr(" ");
 	}
-	if (!nl_flag)
+	if (flag == 0)
 		ft_putstr("\n");
 }
 
@@ -60,22 +63,18 @@ static int	echo_fd_check(void)
  */
 int	ft_echo(t_session *sesh, char **cmd)
 {
-	bool	nl_flag;
-
 	sesh->exit_stat = 0;
 	if (echo_fd_check())
 	{
 		sesh->exit_stat = 1;
 		return (0);
 	}
-	nl_flag = false;
 	if (!(*cmd))
 		ft_putstr("\n");
 	else
 	{
-		if (!ft_strcmp(*(cmd++), "-n"))
-			nl_flag = true;
-		echo_print(cmd, nl_flag);
+		echo_print(cmd, check_flag(sesh, cmd, 'n'), sesh->is_flag_on);
+		sesh->is_flag_on = 0;
 	}
 	return (0);
 }
--- sources/builtins/ft_exit.c
+++ sources/builtins/ft_exit.c
@@ -3,14 +3,41 @@
 /*                                                        :::      ::::::::   */
 /*   ft_exit.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mbarutel <mbarutel@student.42.fr>          +#+  +:+       +#+        */
+/*   By: spuustin <spuustin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/04 08:23:30 by mbarutel          #+#    #+#             */
-/*   Updated: 2023/01/06 13:00:16 by mbarutel         ###   ########.fr       */
+/*   Updated: 2023/01/25 19:17:29 by spuustin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_21sh.h"
+
+static int	get_exit_status(char **commands)
+{
+	int		status;
+
+	if (!commands[1])
+		return (0);
+	status = ft_atoi(commands[1]);
+	if (strlen(commands[1]) > 12 && commands[1][0] != '-' && status < 1)
+	{
+		write(2, "42sh: exit: value bigger than max int, exiting with 1\n", 54);
+		return (1);
+	}
+	if (status == 0)
+	{
+		ft_printf("42sh: exit: %s: numeric argument required\n", commands[1]);
+		return (2);
+	}
+	if (status > 255)
+		return (status % 256);
+	if (status < 0)
+	{
+		status = (status * -1) % 256;
+		return (256 - status);
+	}
+	return (status);
+}
 
 /**
  * It exits the shell
@@ -18,14 +45,23 @@
  * @param sesh The session struct.
  * @param status The exit status of the shell.
  */
-void	ft_exit(t_session *sesh, int status)
+int	ft_exit(t_session *sesh, char **commands)
 {
-	sesh->exit_stat = status;
 	ft_printf("{RED}exit{RESET}\n");
-	ft_history_write_to_file(sesh->term);
-	ft_raw_disable(sesh->orig_termios);
-	if (sesh->term->clipboard.buff)
-		ft_strdel(&sesh->term->clipboard.buff);
-	shell_end_cycle(sesh);
-	exit(status);
+	if (commands[1] && commands[2])
+	{
+		write(2, "42sh: exit: too many arguments\n", 32);
+		sesh->exit_stat = 1;
+		return (0);
+	}
+	else if (commands[1])
+	{	
+		sesh->exit_stat = get_exit_status(commands);
+		ft_history_write_to_file(sesh->term);
+		ft_raw_disable(sesh->orig_termios);
+		if (sesh->term->clipboard.buff)
+			ft_strdel(&sesh->term->clipboard.buff);
+		shell_end_cycle(sesh);
+	}
+	exit(sesh->exit_stat);
 }
