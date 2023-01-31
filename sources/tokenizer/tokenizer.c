8ec2715f1fab2b67b6e0ea56e2234aaf3918dae8
Max <rantil@pm.me>
2023-01-31 15:15:12 +0200

    no print token

--- sources/tokenizer/tokenizer.c
+++ sources/tokenizer/tokenizer.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   tokenizer.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jniemine <jniemine@student.42.fr>          +#+  +:+       +#+        */
+/*   By: mrantil <mrantil@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/21 13:15:33 by jniemine          #+#    #+#             */
-/*   Updated: 2023/01/30 15:02:55 by jniemine         ###   ########.fr       */
+/*   Updated: 2023/01/31 15:14:55 by mrantil          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -89,7 +89,7 @@ t_token	*chop_line(char *line, t_token *args, size_t pointer_n)
 		while (ft_isspace(line[cur]))
 			++cur;
 	}
-	print_tokens(args);
+	// print_tokens(args);
 	ft_strdel(&line);
 	return (args);
 }
