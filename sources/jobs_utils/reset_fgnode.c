ffd91e6993d49f0f4a808cd0de58d8bf907e1851
MrMikey695 <mikeedubas@hotmail.com>
2023-02-01 10:51:01 +0200

    fixed a bug

--- sources/jobs_utils/reset_fgnode.c
+++ sources/jobs_utils/reset_fgnode.c
@@ -6,7 +6,7 @@
 /*   By: mbarutel <mbarutel@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/31 11:26:45 by mike_baru         #+#    #+#             */
-/*   Updated: 2023/02/01 09:02:34 by mbarutel         ###   ########.fr       */
+/*   Updated: 2023/02/01 10:50:09 by mike_baru        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ static void	reset_cmd(char ****cmd)
 	char	***tofree;
 
     tofree = *cmd;
-    while (tofree)
+    while (*tofree)
     {
         ft_arrclean(*tofree);
         ++tofree;
@@ -28,8 +28,10 @@ static void	reset_cmd(char ****cmd)
 void	reset_fgnode(t_shell *sh)
 {
     sh->fg_node->gpid = 0;
-    ft_memdel((void **)&sh->fg_node->pid);
-	reset_cmd(&sh->fg_node->cmd);
+	if (sh->fg_node->pid)
+		ft_memdel((void **)&sh->fg_node->pid);
+	if (sh->fg_node->cmd)
+		reset_cmd(&sh->fg_node->cmd);
 }
 
 void	init_fgnode(t_shell *sh)
