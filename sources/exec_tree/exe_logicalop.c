d8a5b9045372e5395aa3d6907b8925a8d9a890a5
jniemine <nieminen.jakke@gmail.com>
2023-02-03 02:07:58 +0200

    Lots of refactoring on logical op

--- sources/exec_tree/exe_logicalop.c
+++ sources/exec_tree/exe_logicalop.c
@@ -6,36 +6,53 @@
 /*   By: jniemine <jniemine@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/12 19:00:00 by jniemine          #+#    #+#             */
-/*   Updated: 2023/01/29 22:34:31 by jniemine         ###   ########.fr       */
+/*   Updated: 2023/02/03 01:44:57 by jniemine         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_42sh.h"
 
-static void	exe_logical_and(t_logicalop *logicalop, char ***environ_cp,
+static void	exe_logical_or(t_logicalop *logicalop, char ***environ_cp, \
+		char *terminal, t_shell *sh);
+
+/* If exit_stat = 0 execute, if next one is and call this recursively, else if next one is or call it, else call exec_tree*/
+static void	exe_logical_and(t_logicalop *logicalop, char ***environ_cp, \
+		char *terminal, t_shell *sh)
+{
+	ft_printf("exe_logical_and\n");
+	if (sh->exit_stat == 0)
+		exec_tree(logicalop->left, environ_cp, terminal, sh);
+	else if (logicalop->right && logicalop->right->type == LOGICAL_AND)
+		exe_logical_and(((t_logicalop *)logicalop->right), environ_cp, terminal, sh);
+	else if (logicalop->right && logicalop->right->type == LOGICAL_OR)
+		exe_logical_or(((t_logicalop *)logicalop->right), environ_cp, terminal, sh);
+	else
+		exec_tree(logicalop->right, environ_cp, terminal, sh);
+}
+
+/* If exit_stat = 1 execute this, if next one is redir, call this recursively with it, if next one is and call logical_and. If anything else, call exec tree*/
+static void	exe_logical_or(t_logicalop *logicalop, char ***environ_cp, \
+		char *terminal, t_shell *sh)
+{
+	ft_printf("exe_logical_or\n");
+	if (sh->exit_stat == 1)
+		exec_tree(logicalop->left, environ_cp, terminal, sh);
+	else if (logicalop->right && logicalop->right->type == REDIR)
+		exe_logical_or(((t_logicalop *)logicalop->right), environ_cp, terminal, sh);
+	else if (logicalop->right && logicalop->right->type == LOGICAL_OR)
+		exe_logical_and(((t_logicalop *)logicalop->right), environ_cp, terminal, sh);
+	else
+		exec_tree(logicalop->right, environ_cp, terminal, sh);
+}
+
+void	exec_logicalop(t_logicalop *logicalop, char ***environ_cp, \
 		char *terminal, t_shell *sh)
 {
 	exec_tree(logicalop->left, environ_cp, terminal, sh);
-	if (logicalop->right && logicalop->right->type == LOGICAL_OR)
+	if (logicalop->right && logicalop->right->type == LOGICAL_AND)
+		exe_logical_and(((t_logicalop *)logicalop->right), environ_cp, terminal, sh);
+	else if (logicalop->right && logicalop->right->type == LOGICAL_OR)
+		exe_logical_or(((t_logicalop *)logicalop->right), environ_cp, terminal, sh);
+	else
 		exec_tree(logicalop->right, environ_cp, terminal, sh);
-	else if (logicalop->right && sh->exit_stat == 0 && logicalop->right->type == CMD)
-		exec_tree(logicalop->right, environ_cp, terminal, sh);
-}
-
-static void	exe_logical_or(t_logicalop *logicalop, char ***environ_cp,
-		char *terminal, t_shell *sh)
-{
-	if (sh->exit_stat == 0)
-		exec_tree(logicalop->left, environ_cp, terminal, sh);
-	else if (logicalop->right && logicalop->right->type == CMD)
-		exec_tree(logicalop->right, environ_cp, terminal, sh);
-}
-
-void	exec_logicalop(t_logicalop *logicalop, char ***environ_cp,
-		char *terminal, t_shell *sh)
-{
-	if (logicalop->type == LOGICAL_AND)
-		exe_logical_and(logicalop, environ_cp, terminal, sh);
-	else if (logicalop->type == LOGICAL_OR)
-		exe_logical_or(logicalop, environ_cp, terminal, sh);
 }
