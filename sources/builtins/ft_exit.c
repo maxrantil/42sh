e0bd4ca582cd3d787fe253137398e80b99d10c4a
Max <rantil@pm.me>
2023-02-08 12:28:53 +0200

    fix for history in exit

--- sources/builtins/ft_exit.c
+++ sources/builtins/ft_exit.c
@@ -6,7 +6,7 @@
 /*   By: mrantil <mrantil@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/04 08:23:30 by mbarutel          #+#    #+#             */
-/*   Updated: 2023/02/06 12:04:47 by mrantil          ###   ########.fr       */
+/*   Updated: 2023/02/08 12:28:11 by mrantil          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,14 +47,13 @@ static int	get_exit_status(char **commands)
  */
 int	ft_exit(t_shell *sh, char **commands)
 {
-	ft_printf("{RED}exit{RESET}\n");
-	if (commands[1] && commands[2])
+	if (commands[0] && commands[1] && commands[2])
 	{
 		write(2, "42sh: exit: too many arguments\n", 32);
 		sh->exit_stat = 1;
 		return (0);
 	}
-	else if (commands[1])
+	else if (commands[0])
 	{
 		sh->exit_stat = get_exit_status(commands);
 		ft_history_write_to_file(sh->term);
@@ -63,5 +62,6 @@ int	ft_exit(t_shell *sh, char **commands)
 			ft_strdel(&sh->term->clipboard.buff);
 		shell_end_cycle(sh);
 	}
+	ft_printf("{RED}exit{RESET}\n");
 	exit(sh->exit_stat);
 }
