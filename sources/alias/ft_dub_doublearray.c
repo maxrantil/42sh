efbb8b8c6406c7075cb81bbcd2bbdd286080f808
Roope Vuorenlehto <rvuorenl@student.hive.fi>
2023-02-09 13:52:48 +0200

    fix norm ft_dub_doublearray.c

--- sources/alias/ft_dub_doublearray.c
+++ sources/alias/ft_dub_doublearray.c
@@ -6,14 +6,14 @@
 /*   By: rvuorenl <rvuorenl@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/18 14:09:29 by rvuorenl          #+#    #+#             */
-/*   Updated: 2023/02/06 16:47:56 by rvuorenl         ###   ########.fr       */
+/*   Updated: 2023/02/09 13:52:18 by rvuorenl         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_42sh.h"
 #include "libft.h"
 
-int	ft_strarray_size(char **arr);
+int		ft_strarray_size(char **arr);
 char	**ft_create_empty_str_array(int size);
 
 char	**ft_dup_doublearray(char **original)
