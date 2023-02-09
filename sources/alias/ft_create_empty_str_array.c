5e26045a85bef3bf7eb8f08afb03e57fbb6e8172
Roope Vuorenlehto <rvuorenl@student.hive.fi>
2023-02-09 13:54:01 +0200

    fix norm ft_create_empty_str_array.c

--- sources/alias/ft_create_empty_str_array.c
+++ sources/alias/ft_create_empty_str_array.c
@@ -6,7 +6,7 @@
 /*   By: rvuorenl <rvuorenl@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/17 19:28:22 by rvuorenl          #+#    #+#             */
-/*   Updated: 2023/02/06 16:47:52 by rvuorenl         ###   ########.fr       */
+/*   Updated: 2023/02/09 13:53:32 by rvuorenl         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,6 @@ char	**ft_create_empty_str_array(int size)
 	arr = (char **) malloc(sizeof(char *) * (size + 1));
 	if (!arr)
 		exit(2);
-		// ft_exit_error("ft_create_empty_str_array.c", 47);
 	while (size >= 0)
 		arr[size--] = NULL;
 	return (arr);
