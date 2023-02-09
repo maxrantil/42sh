03d5913199525074c18ccb375370d06eaca83ea3
Roope Vuorenlehto <rvuorenl@student.hive.fi>
2023-02-09 11:00:58 +0200

    add match_alias.c

---
+++ sources/alias/match_alias.c
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   match_alias.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rvuorenl <rvuorenl@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/09 10:58:22 by rvuorenl          #+#    #+#             */
+/*   Updated: 2023/02/09 11:00:25 by rvuorenl         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_42sh.h"
+
+//	check if alias found in known aliases, return [pos] OR -1
+int	check_alias_match(char **aliases, char *cmd)
+{
+	int		i;
+	char	*tmp_cmd;
+
+	if (!cmd || !aliases)
+		return (0);
+	i = -1;
+	while (aliases[++i])
+	{
+		tmp_cmd = get_alias_command(aliases[i]);
+		if (ft_strequ(tmp_cmd, cmd) == 1)
+		{
+			ft_strdel(&tmp_cmd);
+			return (i);
+		}
+		ft_strdel(&tmp_cmd);
+	}
+	return (-1);
+}
+
+int	match_first_word(char **alias, char *line)
+{
+	int		pos;
+	char	*first_word;
+
+	if (!line || !alias)
+		return (-1);
+	first_word = get_first_word(line);
+	pos = check_alias_match(alias, first_word);
+	ft_strdel(&first_word);
+	return (pos);
+}
