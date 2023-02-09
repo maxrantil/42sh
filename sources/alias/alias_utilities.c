b5a604b36b7d506fad789cad207bba86542d7c82
Roope Vuorenlehto <rvuorenl@student.hive.fi>
2023-02-09 11:21:21 +0200

    rm alias_utilities2.c, move functions to betterly named files

--- sources/alias/alias_utilities.c
+++ sources/alias/alias_utilities.c
@@ -6,65 +6,66 @@
 /*   By: rvuorenl <rvuorenl@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/08 17:54:18 by rvuorenl          #+#    #+#             */
-/*   Updated: 2023/02/08 18:02:14 by rvuorenl         ###   ########.fr       */
+/*   Updated: 2023/02/09 11:19:02 by rvuorenl         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_42sh.h"
 
-int	check_next_conversion(char *alias)
-{
-	size_t	len;
-
-	if (!alias)
-		return (0);
-	len = ft_strlen(alias);
-	if (len == 0)
-		return (1);
-	else if (ft_iswhitespace(alias[len - 1]))
-		return (1);
-	return (0);
-}
-
-int	skip_to_second_word(char *line)
+int	word_count(char *line)
 {
 	int	i;
+	int	word_count;
 
+	word_count = 0;
 	i = 0;
-	while (line[i] && ft_iswhitespace(line[i]))
+	while (line[i])
+	{
+		while (line[i] && ft_iswhitespace(line[i]))
+			i++;
+		if (line[i])
+		{
+			word_count++;
+			while (line[i] && !(ft_iswhitespace(line[i])))
+				i++;
+			if (line[i])
+				i--;
+			else
+				return (word_count);
+		}
+		else
+			return (word_count);
 		i++;
-	while (line[i] && !(ft_iswhitespace(line[i])))
+	}
+	return (word_count);
+}
+
+ssize_t	find_matching_quote(char *str, char quote)
+{
+	ssize_t	i;
+
+	i = 1;
+	while (str[i])
+	{
+		if (str[i] == '\\' && (str[i + 1] == '\'' || str[i + 1] == '\"'
+				|| str[i + 1] == '\\' ))
+			i++;
+		else if (str[i] == quote)
+			break ;
 		i++;
-	while (line[i] && ft_iswhitespace(line[i]))
-		i++;
+	}
 	return (i);
 }
 
-//	check if alias found in known aliases, return [pos] OR -1
-int	check_alias_match(char **aliases, char *cmd)
+size_t	strip_quotes_single(char *str, size_t quote1)
 {
-	int		i;
-	char	*tmp_cmd;
+	size_t	len;
+	ssize_t	quote2;
 
-	if (!cmd || !aliases)
-		return (-1);
-	i = -1;
-	while (aliases[++i])
-	{
-		tmp_cmd = get_alias_command(aliases[i]);
-		if (ft_strequ(tmp_cmd, cmd) == 1)
-		{
-			ft_strdel(&tmp_cmd);
-			return (i);
-		}
-		ft_strdel(&tmp_cmd);
-	}
-	return (-1);
-}
-
-//	duplicate alias-array without [pos] from original array
-void	dup_arr_rm_pos(char **alias, char ***dup_alias, int pos, int size)
-{
-	*dup_alias = ft_dup_doublearray(alias);
-	remove_alias_single(dup_alias, pos, size);
+	len = ft_strlen(str);
+	quote2 = find_matching_quote(&(str[quote1]), str[quote1]);
+	quote2 += quote1;
+	ft_memmove(&(str[quote1]), &(str[quote1 + 1]), len - quote1);
+	ft_memmove(&(str[quote2 - 1]), &(str[quote2]), len - quote2);
+	return (quote2 - 2);
 }
