80b1adcf4881ac6997d32ab785d5512acbfc497a
Max Rantil <rantil@pm.me>
2023-02-10 17:54:17 +0200

    Revert "Revert "Jniemine fg jobs for builtins""

--- sources/exec_tree/exec_aggregation.c
+++ sources/exec_tree/exec_aggregation.c
@@ -6,7 +6,7 @@
 /*   By: jniemine <jniemine@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/30 20:26:00 by jakken            #+#    #+#             */
-/*   Updated: 2023/02/02 13:41:04 by jniemine         ###   ########.fr       */
+/*   Updated: 2023/02/09 15:49:30 by jniemine         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -70,12 +70,12 @@ void	exec_aggregate(t_aggregate *node, char ***environ_cp,
 		ft_err_print(NULL, "dup2", "failed", 2);
 		return ;
 	}
-	if (sh->pipe->pipefd[1] > 0)
+	if (sh->pipe->write_pipe[1] > 0)
 	{
-		close(sh->pipe->pipefd[1]);
-		sh->pipe->pipefd[1] = -1;
+		close(sh->pipe->write_pipe[1]);
+		sh->pipe->write_pipe[1] = -1;
 	}
 	if (node->cmd && node->cmd->type == CMD)
-		sh->pipe->redirecting = 1;
+		sh->pipe->redir_out = 1;
 	exec_tree(node->cmd, environ_cp, terminal, sh);
 }
