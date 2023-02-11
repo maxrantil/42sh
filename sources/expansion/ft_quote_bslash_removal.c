51d596e940c2ca6cc3c23719ba52510c0d998f0b
mike-ninja <edubas128@gmail.com>
2023-02-11 18:17:58 +0200

    update, need more work to do

--- sources/expansion/ft_quote_bslash_removal.c
+++ sources/expansion/ft_quote_bslash_removal.c
@@ -6,11 +6,28 @@
 /*   By: mbarutel <mbarutel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/17 13:38:31 by mbarutel          #+#    #+#             */
-/*   Updated: 2023/02/11 16:50:31 by mbarutel         ###   ########.fr       */
+/*   Updated: 2023/02/11 18:17:05 by mbarutel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_42sh.h"
+
+static bool	blash_check(char *buff, int pos)
+{
+	int		count;
+
+	if (pos < 0)
+		return (false);
+	count = 0;
+	while (pos >= 0 && buff[pos] == '\\')
+	{
+		count++;
+		pos--;
+	}
+	if (count % 2)
+		return (true);
+	return (false);
+}
 
 static void	ft_qoute_parse(char *buff, char *quote, int len)
 {
@@ -30,7 +47,8 @@ static void	ft_qoute_parse(char *buff, char *quote, int len)
 
 static void	ft_blash_parse(char *buff, char *quote, int len)
 {
-	if (!(*quote) && special_char_check(buff, len, '\\'))
+	// if (!(*quote) && special_char_check(buff, len, '\\'))
+	if (!(*quote) && blash_check(buff, len))
 	{
 		ft_memmove((void *)&buff[len], (void *)&buff[len + 1], \
 		ft_strlen(&buff[len + 1]) + 1);
@@ -68,7 +86,7 @@ void	ft_quote_blash_removal(char *buff)
 	quote = 0;
 	len = ft_strlen(buff);
 	skip = false;
-	while (len >= 0)
+	while (--len >= 0)
 	{
 		if ((buff[len] == S_QUO || buff[len] == D_QUO))
 			handle_quotes(buff, len, &quote, &skip);
@@ -76,6 +94,5 @@ void	ft_quote_blash_removal(char *buff)
 			ft_blash_parse(buff, &quote, len);
 		else if (buff[len] == '\n' && len && special_char_check(buff, len, '\\') && !skip)
 			handle_newline(buff, len);
-		len--;
 	}
 }
