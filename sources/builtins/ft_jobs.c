8bb4ff9e827cb715d31a8d4f075460a4ddc64c14
MrMikey695 <mikeedubas@hotmail.com>
2023-02-11 21:32:00 +0200

    jobs looking good. Now need cleaning

--- sources/builtins/ft_jobs.c
+++ sources/builtins/ft_jobs.c
@@ -3,16 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   ft_jobs.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: spuustin <spuustin@student.42.fr>          +#+  +:+       +#+        */
+/*   By: mbarutel <mbarutel@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/01 15:00:54 by mbarutel          #+#    #+#             */
-/*   Updated: 2023/02/11 14:10:27 by spuustin         ###   ########.fr       */
+/*   Updated: 2023/02/11 21:30:42 by mbarutel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_42sh.h"
 
-static int	validate_options(t_shell *session, char **commands)
+static int	validate_options(t_shell *sh, char **commands)
 {
 	int		i;
 
@@ -24,76 +24,66 @@ static int	validate_options(t_shell *session, char **commands)
 			i++;
 			break;
 		}
-		if (str_only_contains_chars(commands[i], "lp", session) == 0)
-		{
-			print_usage("jobs", session->option);
-			return (0);
-		}
-		if (commands[i][0] != '-' || \
-		!str_only_contains_chars(commands[i], "lp", session))
+		if (commands[i][0] != '-')
 			break;
-		session->option = commands[i][ft_strlen(commands[i]) - 1];
+		else if (str_only_contains_chars(commands[i], "lp", sh) == 0)
+			return (0);
+		sh->option = commands[i][ft_strlen(commands[i]) - 1];
 		i++;
 	}
-	session->option_count = i - 1;
+	sh->option_count = i - 1;
 	return (1);
 }
 
-static void	display_no_flags(t_shell *sh)
+static void	display_job_with_flags(t_shell *sh, t_bg_jobs *job)
 {
-	t_bg_jobs *job;
-
-	job = sh->bg_node;
-	while (job)
-	{
-        display_job_node(sh, job);
-		if (job->status == DONE || job->status == TERMINATED)
-			bg_node_delete(sh, &job);
-		else
-			job = job->next;
-	}
-}
-
-static void	display_l_flags(t_shell *sh)
-{
-	t_bg_jobs	*job;
-
-	job = sh->bg_node;
-	while (job)
-	{
+	if (sh->option == 'l')
 		display_job_pipeline(sh, job);
-		if (job->status == DONE || job->status == TERMINATED)
-			bg_node_delete(sh, &job);
-		else
-			job = job->next;
-	}
+	else if (sh->option == 'p')
+		ft_printf("%d\n", *job->pid);
+	else
+		display_job_node(sh, job);
 }
 
-static void	display_p_flags(t_shell *sh)
+static void	display_fetched_jobs(t_shell *sh, char **cmd)
 {
+	int			i;
 	t_bg_jobs	*job;
 
-	job = sh->bg_node;
-	while (job)
+	i = -1;
+	job = NULL;
+	while (cmd[++i])
 	{
-		ft_printf("%d\n", *job->pid);
-		if (job->status == DONE || job->status == TERMINATED)
-			bg_node_delete(sh, &job);
+		if (*(cmd[i]) == '-')
+			job = sh->bg_node;
 		else
-			job = job->next;
+			job = bg_fetch_node(sh, &cmd[i]);
+		if (job)
+			display_job_with_flags(sh, job);
+		++i;
 	}
 }
 
 int	ft_jobs(t_shell *sh, char **commands)
 {
+	t_bg_jobs	*job;
+	
 	if (!validate_options(sh, commands))
 		return (0);
-	if (sh->option == 'l')
-		display_l_flags(sh);
-	else if (sh->option == 'p')
-		display_p_flags(sh);
-	else
-		display_no_flags(sh);
+	if (commands[sh->option_count + 1])
+	{
+		display_fetched_jobs(sh, &commands[sh->option_count]);
+		return (0);
+	}
+	job = sh->bg_node;
+	while (job)
+	{
+		display_job_with_flags(sh, job);	
+		if (job->status == DONE || job->status == TERMINATED)
+			bg_node_delete(sh, &job);
+		else
+			job = job->next;
+	}
 	sh->option_count = 0;
 	sh->option = 'e';
 	return (0);
