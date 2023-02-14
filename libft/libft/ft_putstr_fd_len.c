ac5c966cb3006910638b49dde98c1179cde2f179
mike-ninja <edubas128@gmail.com>
2023-02-14 10:43:29 +0200

    norminetted libft

--- libft/libft/ft_putstr_fd_len.c
+++ libft/libft/ft_putstr_fd_len.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_putstr_fd_len.c                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mbarutel <mbarutel@student.hive.fi>        +#+  +:+       +#+        */
+/*   By: mbarutel <mbarutel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/13 21:40:17 by mbarutel          #+#    #+#             */
-/*   Updated: 2023/02/13 21:44:25 by mbarutel         ###   ########.fr       */
+/*   Updated: 2023/02/14 10:42:21 by mbarutel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@
  * @param fd The file descriptor to write to.
  * @param len The length of the string to be printed.
  */
-void    ft_putstr_fd_len(char *str, int fd, int len)
+void	ft_putstr_fd_len(char *str, int fd, int len)
 {
 	int ignore	__attribute__((unused));
 
