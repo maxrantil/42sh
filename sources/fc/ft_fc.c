09443d1333296d1396950412e442e17a48d0c6dc
mike-ninja <edubas128@gmail.com>
2023-02-15 17:00:14 +0200

    updated fc, now should be good to go

--- sources/fc/ft_fc.c
+++ sources/fc/ft_fc.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_fc.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mrantil <mrantil@student.hive.fi>          +#+  +:+       +#+        */
+/*   By: mbarutel <mbarutel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/16 09:41:05 by mrantil           #+#    #+#             */
-/*   Updated: 2023/02/15 16:23:56 by mrantil          ###   ########.fr       */
+/*   Updated: 2023/02/15 16:51:59by mbarutel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,26 @@ static void	fc_init_flags(t_fc *fc)
 	fc->end = 0;
 }
 
+static bool	validate_input(char ***cmd, t_fc *fc)
+{
+	int		i;
+	int		j;
+
+	i = fc->flags;
+	while ((*cmd)[i])
+	{
+		j = 0;
+		while ((*cmd)[i][j])
+		{
+			if (!ft_isdigit((*cmd)[i][j]) && ((*cmd)[i][j] != '-' && !j))
+				return (false);
+			j++;
+		}		
+		i++;
+	}
+	return (true);
+}
+
 int	ft_fc(t_shell *sh, char ***cmd)
 {
 	t_fc	fc;
@@ -35,13 +55,17 @@ int	ft_fc(t_shell *sh, char ***cmd)
 	ret = 0;
 	if (fc.e && !(*cmd)[fc.flags])
 		return (fc_usage("-e: option requires an argument"));
-	else if (fc.e)
-		fc.flags++;
 	if (fc.s && !fc.e)
 		return (fc_s_flag(sh, &fc, cmd));
 	else if (fc.e && !fc.l)
 		ret = fc_no_flag_or_e_flag(sh, &fc, cmd);
 	else if (fc.l)
+	{
+		if (fc.e)
+			fc.flags++;
+		if (!validate_input(cmd, &fc))
+			return (fc_usage("invalid input"));
 		return (fc_list_flags(sh, &fc, cmd));
+	}
 	return (ret);
 }
