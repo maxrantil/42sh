fcc9de3b36bf12b53277bf15bb76e1f48fc67492
Roope Vuorenlehto <rvuorenl@student.hive.fi>
2023-02-20 15:19:26 +0200

    optimize history, now instead of reallocating every command, allocate MAX_HISTORY and when history is full, delete first half and move second half to beginning

--- sources/keyboard/ft_history_add.c
+++ sources/keyboard/ft_history_add.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_history_add.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mrantil <mrantil@student.hive.fi>          +#+  +:+       +#+        */
+/*   By: rvuorenl <rvuorenl@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/31 12:38:01 by mrantil           #+#    #+#             */
-/*   Updated: 2023/01/31 12:47:38 by mrantil          ###   ########.fr       */
+/*   Updated: 2023/02/20 15:16:13 by rvuorenl         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,51 +14,19 @@
 
 extern t_shell	*g_sh;
 
-static void	add_to_capped_history(t_term *t, char *command)
+static void	add_to_capped_history(t_term *t)
 {
-	char	**temp;
-	int		i;
 	int		j;
 
-	temp = (char **)malloc(sizeof(char *) * (MAX_HISTORY + 1));
-	if (!temp)
-	{
-		ft_putendl_fd("42sh: malloc error, add_to_capped_history()", 2);
-		ft_raw_disable(g_sh->orig_termios);
-		exit(1);
-	}
-	i = MAX_HISTORY / 2;
 	j = 0;
-	while (t->history_arr[++i])
-		temp[j++] = ft_strdup(t->history_arr[i]);
-	temp[j++] = ft_strdup(command);
-	temp[j] = NULL;
+	while (j < (MAX_HISTORY / 2))
+	{
+		ft_strdel(&(t->history_arr[j]));
+		t->history_arr[j] = t->history_arr[j + (MAX_HISTORY / 2)];
+		t->history_arr[j + (MAX_HISTORY / 2)] = NULL;
+		j++;
+	}
 	t->history_size = j;
-	ft_arrclean(t->history_arr);
-	t->history_arr = temp;
-}
-
-static void	increase_history_size(t_term *t)
-{
-	char	**temp;
-	int		i;
-
-	temp = (char **)malloc(sizeof(char *) * (t->history_size + 2));
-	if (temp)
-	{
-		i = -1;
-		while (t->history_arr[++i])
-			temp[i] = ft_strdup(t->history_arr[i]);
-		temp[i] = NULL;
-		ft_arrclean(t->history_arr);
-		t->history_arr = temp;
-	}
-	else
-	{
-		ft_putendl_fd("42sh: malloc error, increase_history_size()", 2);
-		ft_raw_disable(g_sh->orig_termios);
-		exit(1);
-	}
 }
 
 void	ft_history_add_command(t_term *t, char *command)
@@ -72,11 +40,9 @@ void	ft_history_add_command(t_term *t, char *command)
 			i++;
 		if (!command[i])
 			return ;
-		increase_history_size(t);
 		t->history_arr[t->history_size] = ft_strdup(command);
 		t->history_size++;
-		t->history_arr[t->history_size] = NULL;
 	}
 	else
-		add_to_capped_history(t, command);
+		add_to_capped_history(t);
 }
