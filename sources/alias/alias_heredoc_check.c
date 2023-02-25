80e14cb0af51650cab63603115c0957bd9d9a9d7
jniemine <jniemine@student.hive.fi>
2023-02-25 15:48:23 +0200

    normed

--- sources/alias/alias_heredoc_check.c
+++ sources/alias/alias_heredoc_check.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   alias_heredoc_check.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mbarutel <mbarutel@student.42.fr>          +#+  +:+       +#+        */
+/*   By: jniemine <jniemine@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/22 16:03:33 by mbarutel          #+#    #+#             */
-/*   Updated: 2023/02/22 16:19:34 by mbarutel         ###   ########.fr       */
+/*   Updated: 2023/02/25 15:33:04 by jniemine         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,9 +23,9 @@ void	alias_heredoc_check(char **line)
 	count = 0;
 	while ((*line)[++i])
 	{
-		if ((*line)[i] == '<')
+		if ((*line)[i] == '<' && (*line)[i + 1] == '<')
 			++count;
-		if (count == 2)
+		if (count == 1)
 		{
 			ft_memmove(&(*line)[i], &(*line)[i + 1],
 				ft_strlen(&(*line)[i + 1]) + 1);
