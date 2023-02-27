bd9fff192ffbf8d31da9c2b4b91cc80d5b7a7224
Max Rantil <rantil@pm.me>
2023-02-27 23:34:36 +0200

    Update ft_freeda.c

--- libft/libft/ft_freeda.c
+++ libft/libft/ft_freeda.c
@@ -17,7 +17,7 @@ int	ft_freeda(void ***a, size_t row)
 	unsigned int	i;
 
 	i = -1;
-	if (!a || !*a || !(*a)[0])
+	if (!a || !*a)
 		return (1);
 	while ((*a)[++i] && i < row)
 		ft_strdel((char **)&(*a)[i]);
